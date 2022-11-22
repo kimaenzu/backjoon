5f2c96733818d997ef3215db41229938782657a6
kimaenzu <khj5606kh@gmail.com>
2022-11-22 23:26:22 +0900

    for문을 사용하지 않도록 수정 결과 : 틀렸습니다

--- 9935.py
+++ 9935.py
@@ -1,13 +1,20 @@
 sen = str(input())
 boom = str(input())
-count = 1
+count = 2
 len_b = len(boom)
+i = 0
+pre_sen = 'a'
 while (count !=0) :
-    count = 0
-    for i in range(len(sen)-len(boom)+1):
-        if (sen[i:len_b+i] == boom) ==True:
-            sen = sen[:i]+sen[len_b+i:]
-            count += 1
+    if (sen[i:len_b+i] == boom) ==True:
+        pre_sen = sen
+        sen = sen[:i]+sen[len_b+i:]
+    else:
+        i += 1
+    if (len(sen)-len(boom)+1 == i):
+        i = 0
+        count -= 1
+    if sen[i:len_b+i] == '':
+        count = 0
 if sen == '':
     print("FRULA")
 else:
